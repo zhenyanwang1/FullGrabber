9ca3ca19bff3a51d0323f79aacc4fe3a6f17bced
Zhenyan <zhenyanwang1@gmail.com>
2014-08-30 18:13:52 +0800

    Close mongodb connections.

--- web.py
+++ web.py
@@ -10,6 +10,7 @@ class MainHandler(tornado.web.RequestHandler):
         db = client["zhihu"]
         data = db["questions"].find()
         self.render("main.html", questions=data)
+        client.close()
 
 
 class QuestionHandler(tornado.web.RequestHandler):
@@ -20,7 +21,7 @@ class QuestionHandler(tornado.web.RequestHandler):
         comments = db["comments"].find({"ParentType": 1, "ParentID": int(question)}).sort("Seq", ASCENDING)
         answers = db["answers"].find({"QuestionID": int(question)}).sort("Seq", ASCENDING)
         self.render("question.html", title=data["Title"], content=data["Content"], comments=comments, answers=answers)
-
+        client.close()
 
 class AnswerHandler(tornado.web.RequestHandler):
     def get(self, answer):
@@ -29,6 +30,7 @@ class AnswerHandler(tornado.web.RequestHandler):
         data = db["answers"].find_one({"AnswerID": int(answer)})
         comments = db["comments"].find({"ParentType": 2, "ParentID": int(answer)}).sort("Seq", ASCENDING)
         self.render("answer.html", answer_id=data["AnswerID"], content=data["Content"], comments=comments)
+        client.close()
 
 
 class ImageHandler(tornado.web.RequestHandler):
@@ -37,6 +39,7 @@ class ImageHandler(tornado.web.RequestHandler):
         db = client["zhihu"]
         data = db["images"].find_one({"_id": ObjectId(image_id)})
         self.write(data["Data"])
+        client.close()
 
 
 application = tornado.web.Application([
